433355041a5dc4425fe468e1cf4d62ed5b78f2b8
Michael Marino <mmarino@gmail.com>
2014-04-04 15:14:52 +0200

    Update with version 1.1.0 of datebox.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 kanso-jquery-datebox
 ====================
 
-Kan.so export of jquery datebox
+jQuery Mobile datebox wrapped for kan.so
+
+This is currently version 1.1.0.  The Datebox website is: 
+
+http://dev.jtsage.com/jQM-DateBox/
+
---
+++ build_datebox.py
@@ -0,0 +1,65 @@
+"""
+Builds datebox into one file.  To build a new version, change the version
+number and rerun.
+
+M. Marino Apr 2014
+"""
+
+import urllib2 
+import sys
+import re
+
+base_url = "http://dev.jtsage.com/cdn/datebox"
+version = "1.1.0"
+module_name = "jquery-mobile-datebox"
+json_file = """
+{
+    "name": "%(NAME)s",
+    "version": "%(VERSION)s-kanso.1",
+    "categories": ["utils"],
+    "maintainers": [
+        {
+            "name": "Michael Marino",
+            "url": "https://github.com/mgmarino"
+        }
+    ],
+    "url": "%(URL)s",
+    "modules": "%(NAME)s.js",
+    "modules_attachment": false,
+    "dependencies": {
+        "modules": ">=0.0.8",
+        "jquery": ">=1.9.1-kanso.2",
+        "jquery-mobile": ">=1.4.2-kanso.1"
+    },
+    "description": "A Date and Time Picker plugin for jQueryMobile 1.2.0+."
+}
+""" % { 
+        "NAME" : module_name, "VERSION" : version,
+        "URL" : "http://dev.jtsage.com/jQM-DateBox/"
+      }
+
+components = [
+  "calbox",
+  "datebox",
+  "flipbox",
+  "durationbox",
+  "slidebox",
+]
+  
+total_url = "%(BASE)s/%(VERSION)s/jqm-datebox-%(VERSION)s" % { "BASE" : base_url, "VERSION" : version }
+
+core_data = "" 
+total_components = ["core"]
+total_components.extend(["mode." + k for k in components])
+for comp in total_components: 
+    t = total_url + ".%s.js" % comp
+    try:
+        dat = urllib2.urlopen(t)
+        core_data += dat.read().replace('( jQuery )', '( require("jquery") )') 
+    except urllib2.URLError, e:
+        print e, t
+        print "Exiting..."
+        sys.exit(1)
+
+open(module_name + ".js", "w").write(core_data)
+open("kanso.json", "w").write(json_file)
